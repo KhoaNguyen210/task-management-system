c244e32d1cde2bfd2fb93aa0b5a3453ce131f72d
trietcs <triet3101@gmail.com>
2025-05-09 10:23:00 +0700

    Update TaskEvaluated

--- app/Notifications/TaskEvaluated.php
+++ app/Notifications/TaskEvaluated.php
@@ -17,18 +17,18 @@ class TaskEvaluated extends Notification
         $this->task = $task;
     }
 
-    public function via($notifiable)
+    public function via($notifiable): array
     {
         return ['mail'];
     }
 
-    public function toMail($notifiable)
+    public function toMail($notifiable): MailMessage
     {
         return (new MailMessage)
-                    ->subject('Công việc đã được đánh giá: ' . $this->task->title)
-                    ->line('Công việc "' . $this->task->title . '" của bạn đã được Trưởng Bộ môn đánh giá.')
-                    ->line('Mức độ hoàn thành: ' . $this->task->evaluation_level)
-                    ->line('Nhận xét: ' . ($this->task->evaluation_comment ?? 'Không có nhận xét'))
-                    ->action('Xem chi tiết', url('/tasks/' . $this->task->id . '/show'));
+            ->subject('Công việc đã được đánh giá: ' . $this->task->title)
+            ->line('Công việc "' . $this->task->title . '" của bạn đã được Trưởng Bộ môn đánh giá.')
+            ->line('Mức độ hoàn thành: ' . $this->task->evaluation_level)
+            ->line('Nhận xét: ' . ($this->task->evaluation_comment ?? 'Không có nhận xét'))
+            ->action('Xem chi tiết', url('/tasks/' . $this->task->id . '/show'));
     }
 }
