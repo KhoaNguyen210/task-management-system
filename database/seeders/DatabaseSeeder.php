37bae43a58d36e8ce5b393904a882ab8bbac3610
KhoaNguyen210 <96481978+KhoaNguyen210@users.noreply.github.com>
2025-06-23 15:13:07 +0700

    Del Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Database\Seeders\DepartmentSeeder;
 use Database\Seeders\UserSeeder;
-use Database\Seeders\TaskDataSeeder;
-use Database\Seeders\TaskAssignmentDataSeeder;
-use Database\Seeders\TaskProgressDataSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,13 +18,6 @@ class DatabaseSeeder extends Seeder
         // Gọi UserSeeder sau để tạo người dùng và cập nhật head_id cho departments
         $this->call([
             UserSeeder::class,
-        ]);
-
-        // Gọi các seeder mới cho tasks, task_assignments, task_progress
-        $this->call([
-            TaskDataSeeder::class,
-            TaskAssignmentDataSeeder::class,
-            TaskProgressDataSeeder::class,
         ]);
     }
 }
